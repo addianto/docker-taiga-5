5f0ca4d95c16e06cb5126860cd877061f94f1a0b
Daya Adianto <dayaadianto@cs.ui.ac.id>
2021-01-04 00:42:37 +0700

    Allow backend configuration via environment variables

--- docker-compose/settings.py
+++ docker-compose/settings.py
@@ -1,18 +1,21 @@
-SECRET_KEY = '9%pno@m688el28@2+^y4v^&6wluqk-g#j#d7$dsjtht)o30dn1'
+import os
+
+SECRET_KEY = os.getenv('TAIGA_SECRET_KEY', '9%pno@m688el28@2+^y4v^&6wluqk-g#j#d7$dsjtht)o30dn1')
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
+    os.getenv('TAIGA_ALLOWED_HOST', 'localhost'),
 ]
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'taiga',
-        'USER': 'taiga',
-        'PASSWORD': 'changeme',
-        'HOST': 'database',
-        'PORT': '5432',
+        'NAME': os.getenv('TAIGA_DB_NAME', 'taiga'),
+        'USER': os.getenv('TAIGA_DB_USER', 'taiga'),
+        'PASSWORD': os.getenv('TAIGA_DB_PASSWORD', 'changeme'),
+        'HOST': os.getenv('TAIGA_DB_HOST', 'database'),
+        'PORT': os.getenv('TAIGA_DB_PORT', '5432'),
     }
 }
 
